4a5ecbda987981782abe14ee3425ab359bc42536
Matteo Vescovi <matteo.vescovi@yahoo.co.uk>
2007-07-10 19:51:01 +0000

    Implemented base Combiner class, meritocracy policy combiner, and added unit tests - adding files.

---
+++ src/core/meritocracyCombiner.h
@@ -0,0 +1,50 @@
+
+/*****************************************************************************\
+ *                                                                           *
+ * Soothsayer, an extensible predictive text entry system                    *
+ * ------------------------------------------------------                    *
+ *                                                                           *
+ * Copyright (C) 2004  Matteo Vescovi <matteo.vescovi@yahoo.co.uk>           *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or             *
+ * modify it under the terms of the GNU General Public License               *
+ * as published by the Free Software Foundation; either version 2            *
+ * of the License, or (at your option) any later version.                    *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ * GNU General Public License for more details.                              *
+ *                                                                           *
+ * You should have received a copy of the GNU General Public License         *
+ * along with this program; if not, write to the Free Software               *
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.*
+ *                                                                           *
+\*****************************************************************************/
+
+
+#ifndef SOOTH_MERITOCRACYCOMBINER
+#define SOOTH_MERITOCRACYCOMBINER
+
+#include "combiner.h"
+
+/** MeritocracyCombiner.
+ *
+ * Meritocracy is a system of government or other organization based
+ * on demonstrated ability (merit) and talent rather than by wealth
+ * (plutocracy), family connections (nepotism), class privilege,
+ * cronyism, popularity (as in democracy) or other historical
+ * determinants of social position and political power.
+ *
+ */
+class MeritocracyCombiner : public Combiner {
+public:
+    MeritocracyCombiner();
+    virtual ~MeritocracyCombiner();
+    virtual Prediction combine(const std::vector< Prediction >&);
+private:
+
+};
+
+
+#endif // SOOTH_MERITOCRACYCOMBINER
