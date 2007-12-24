f1a8cbe82cc84b0e2903249ecf5823a48ce57fa4
Matteo Vescovi <matteo.vescovi@yahoo.co.uk>
2007-12-24 22:11:22 +0000

    Initial stab at creating a Debian package.

---
+++ debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the soothsayer package
+#
+0 4	* * *	root	soothsayer_maintenance
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(soothsayer):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
+  title="soothsayer" command="/usr/bin/soothsayer"
