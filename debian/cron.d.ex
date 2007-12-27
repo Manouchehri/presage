c0621a19332b7cecc8ece03a2d90cea55c326e42
Matteo Vescovi <matteo.vescovi@yahoo.co.uk>
2007-12-27 18:25:03 +0000

    More changes to Debian packaging control files.

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the soothsayer package
-#
-0 4	* * *	root	soothsayer_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(soothsayer):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="soothsayer" command="/usr/bin/soothsayer"
