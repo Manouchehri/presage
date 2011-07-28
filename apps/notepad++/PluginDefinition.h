49429b96439fb413d5256edd3a9c308943005f30
Matteo Vescovi <matteo.vescovi@yahoo.co.uk>
2011-07-28 19:37:43 +0000

    fixing up license headers.

--- apps/notepad++/PluginDefinition.h
+++ apps/notepad++/PluginDefinition.h
@@ -1,19 +1,27 @@
-//this file is part of notepad++
-//Copyright (C)2003 Don HO <donho@altern.org>
-//
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either
-//version 2 of the License, or (at your option) any later version.
-//
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
-//
-//You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+/******************************************************
+ *  Presage, an extensible predictive text entry system
+ *  ---------------------------------------------------
+ *
+ *  Copyright (C) 2008  Matteo Vescovi <matteo.vescovi@yahoo.co.uk>
+    Copyright (C) 2003  Don HO <donho@altern.org>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+                                                                             *
+                                                                **********(*)*/
+
 
 #ifndef PLUGINDEFINITION_H
 #define PLUGINDEFINITION_H
@@ -28,7 +36,7 @@
 //-------------------------------------//
 // Here define your plugin name
 //
-const TCHAR NPP_PLUGIN_NAME[] = TEXT("Presage");
+const TCHAR NPP_PLUGIN_NAME[] = TEXT("NppPresage");
 
 //-----------------------------------------------//
 //-- STEP 2. DEFINE YOUR PLUGIN COMMAND NUMBER --//
