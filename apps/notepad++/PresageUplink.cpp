b0a62238e185081d68c6c9fb7bb359d7af1bcb8f
Matteo Vescovi <matteo.vescovi@yahoo.co.uk>
2011-09-05 17:24:13 +0000

    began implementing dynamic loading of libpresage.

---
+++ apps/notepad++/PresageUplink.cpp
@@ -0,0 +1,102 @@
+
+/******************************************************
+ *  Presage, an extensible predictive text entry system
+ *  ---------------------------------------------------
+ *
+ *  Copyright (C) 2011  Matteo Vescovi <matteo.vescovi@yahoo.co.uk>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+                                                                             *
+                                                                **********(*)*/
+
+
+#include "PresageUplink.h"
+#include <tchar.h>
+#include <windows.h>
+#include <Shlwapi.h>
+
+
+HINSTANCE   hInstLib = NULL; 
+
+
+PFUNC_presage_completion presage_completion = NULL;
+PFUNC_presage_config presage_config = NULL;
+PFUNC_presage_config_set presage_config_set = NULL;
+PFUNC_presage_context presage_context = NULL;
+PFUNC_presage_context_change presage_context_change = NULL;
+PFUNC_presage_free presage_free = NULL;
+PFUNC_presage_free_string presage_free_string = NULL;
+PFUNC_presage_free_string_array presage_free_string_array = NULL;
+PFUNC_presage_new presage_new = NULL;
+PFUNC_presage_new_with_config presage_new_with_config = NULL;
+PFUNC_presage_predict presage_predict = NULL;
+PFUNC_presage_prefix presage_prefix = NULL;
+PFUNC_presage_save_config presage_save_config = NULL;
+
+
+bool LoadPresageDLL(void)
+{
+    bool    bRet = FALSE;
+    HKEY    hKey = NULL;
+    DWORD   size = MAX_PATH;
+    TCHAR   pszPath[MAX_PATH];
+	HMODULE hModule = NULL;
+
+    pszPath[0] = '\0';
+
+    if (ERROR_SUCCESS == ::RegOpenKeyEx(HKEY_CURRENT_USER, _T("Software\\Presage"), 0, KEY_READ, &hKey))
+    {
+		if (ERROR_SUCCESS == ::RegQueryValueEx(hKey, _T(""), NULL ,NULL, (LPBYTE)pszPath, &size))
+			_tcscat(pszPath, _T("bin\\libpresage-1.dll"));
+		::RegCloseKey(hKey);
+	}
+	else
+	{
+        /* module path of notepad */
+		GetModuleFileName((HMODULE)hModule, pszPath, sizeof(pszPath));
+		PathRemoveFileSpec(pszPath);
+		PathRemoveFileSpec(pszPath);
+		PathAppend(pszPath, _T("\\libpresage-1.dll"));
+	}
+
+	hInstLib = LoadLibrary(pszPath);
+
+	if (hInstLib != NULL)
+	{
+		presage_completion = (PFUNC_presage_completion) GetProcAddress (hInstLib, "presage_completion");
+		presage_config = (PFUNC_presage_config) GetProcAddress (hInstLib, "presage_config");
+		presage_config_set = (PFUNC_presage_config_set) GetProcAddress (hInstLib, "presage_config_set");
+		presage_context = (PFUNC_presage_context) GetProcAddress (hInstLib, "presage_context");
+		presage_context_change = (PFUNC_presage_context_change) GetProcAddress (hInstLib, "presage_context_change");
+		presage_free = (PFUNC_presage_free) GetProcAddress (hInstLib, "presage_free");
+		presage_free_string = (PFUNC_presage_free_string) GetProcAddress (hInstLib, "presage_free_string");
+		presage_free_string_array = (PFUNC_presage_free_string_array) GetProcAddress (hInstLib, "presage_free_string_array");
+		presage_new = (PFUNC_presage_new) GetProcAddress (hInstLib, "presage_new");
+		presage_new_with_config = (PFUNC_presage_new_with_config) GetProcAddress (hInstLib, "presage_new_with_config");
+		presage_predict = (PFUNC_presage_predict) GetProcAddress (hInstLib, "presage_predict");
+		presage_prefix = (PFUNC_presage_prefix) GetProcAddress (hInstLib, "presage_prefix");
+		presage_save_config = (PFUNC_presage_save_config) GetProcAddress (hInstLib, "presage_save_config");
+
+		bRet = TRUE;
+	}
+
+    return bRet;
+}
+
+void UnloadPresageDLL(void)
+{
+    if (hInstLib != NULL)
+        FreeLibrary(hInstLib);
+}
---
+++ apps/notepad++/PresageUplink.h
@@ -0,0 +1,112 @@
+
+/******************************************************
+ *  Presage, an extensible predictive text entry system
+ *  ---------------------------------------------------
+ *
+ *  Copyright (C) 2011  Matteo Vescovi <matteo.vescovi@yahoo.co.uk>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+                                                                             *
+                                                                **********(*)*/
+
+
+#ifndef PRESAGE_UPLINK_H
+#define PRESAGE_UPLINK_H
+
+/* must match declarations in presageException.h */
+typedef enum
+{
+	PRESAGE_OK = 0,    /* successful result */
+	PRESAGE_ERROR,     /* generic/unknown presage error */
+	PRESAGE_TOKEN_PREFIX_MISMATCH_ERROR,
+	PRESAGE_SMOOTHED_NGRAM_PREDICTOR_LEARN_ERROR,
+	PRESAGE_CONFIG_VARIABLE_ERROR,
+	PRESAGE_INVALID_CALLBACK_ERROR,
+	PRESAGE_INVALID_SUGGESTION_ERROR,
+	PRESAGE_INIT_PREDICTOR_ERROR,
+	PRESAGE_SQLITE_OPEN_DATABASE_ERROR,
+	PRESAGE_SQLITE_EXECUTE_SQL_ERROR
+} presage_error_code_t;
+
+/* must match declarations in presageCallback.h */
+typedef const char* (*_presage_callback_get_past_stream)   (void*);
+typedef const char* (*_presage_callback_get_future_stream) (void*);
+
+/* must match declarations in presage.h */
+typedef struct _presage* presage_t;
+
+typedef presage_error_code_t (__cdecl * PFUNC_presage_new)
+                             (_presage_callback_get_past_stream past_stream_cb,
+						      void* past_stream_cb_arg,
+						      _presage_callback_get_future_stream future_stream_cb,
+						      void* future_stream_cb_arg,
+						      presage_t* result);
+    
+typedef presage_error_code_t (__cdecl * PFUNC_presage_new_with_config)
+	                         (_presage_callback_get_past_stream past,
+						      void* past_stream_cb_arg,
+						      _presage_callback_get_future_stream future_stream_cb,
+						      void* future_stream_cb_arg,
+						      const char* config,
+						      presage_t* result);
+    
+typedef void                 (__cdecl * PFUNC_presage_free)
+                             (presage_t prsg);
+    
+typedef void                 (__cdecl * PFUNC_presage_free_string)
+	                         (char* str);
+
+typedef void                 (__cdecl * PFUNC_presage_free_string_array)
+	                         (char** str);
+
+typedef presage_error_code_t (__cdecl * PFUNC_presage_predict)
+	                         (presage_t prsg,
+						      char*** result);
+    
+typedef presage_error_code_t (__cdecl * PFUNC_presage_completion)
+	                         (presage_t prsg,
+						      const char* token,
+						      char** result);
+
+typedef presage_error_code_t (__cdecl * PFUNC_presage_context)
+	                         (presage_t prsg, 
+						      char** result);
+
+typedef presage_error_code_t (__cdecl * PFUNC_presage_context_change)
+	                         (presage_t prsg,
+						      int* result);
+
+typedef presage_error_code_t (__cdecl * PFUNC_presage_prefix)
+	                         (presage_t prsg,
+						      char** result);
+
+typedef presage_error_code_t (__cdecl * PFUNC_presage_config)
+	                         (presage_t prsg,
+						      const char* variable,
+						      char** result);
+
+typedef presage_error_code_t (__cdecl * PFUNC_presage_config_set)
+	                         (presage_t prsg,
+						      const char* variable,
+						      const char* value);
+    
+typedef presage_error_code_t (__cdecl * PFUNC_presage_save_config)
+	                         (presage_t prsg);
+
+
+bool LoadPresageDLL(void);
+void UnloadPresageDLL(void);
+
+#endif /* PRESAGE_UPLINK_H */
