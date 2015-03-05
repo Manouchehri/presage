64bf5946ffbe7e275e09b571e261b5febd4a21fc
Matteo Vescovi <matteo.vescovi@yahoo.co.uk>
2015-03-05 20:25:37 +0000

    add license and close buttons to about dialog.

--- bindings/csharp/presage_wcf_service_system_tray/AboutForm.Designer.cs
+++ bindings/csharp/presage_wcf_service_system_tray/AboutForm.Designer.cs
@@ -60,8 +60,12 @@ namespace presage_wcf_service_system_tray
             this.disclaimerLabel = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.linkLabel = new System.Windows.Forms.LinkLabel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.licenseButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
             this.flowLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.presageLogoBox)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
@@ -74,12 +78,13 @@ namespace presage_wcf_service_system_tray
             this.flowLayoutPanel1.Controls.Add(this.disclaimerLabel);
             this.flowLayoutPanel1.Controls.Add(this.label1);
             this.flowLayoutPanel1.Controls.Add(this.linkLabel);
+            this.flowLayoutPanel1.Controls.Add(this.tableLayoutPanel1);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(10);
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 350);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 375);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // presageLogoBox
@@ -168,7 +173,7 @@ namespace presage_wcf_service_system_tray
             | System.Windows.Forms.AnchorStyles.Right)));
             this.linkLabel.AutoSize = true;
             this.linkLabel.Location = new System.Drawing.Point(10, 303);
-            this.linkLabel.Margin = new System.Windows.Forms.Padding(0, 0, 0, 10);
+            this.linkLabel.Margin = new System.Windows.Forms.Padding(0, 0, 0, 20);
             this.linkLabel.Name = "linkLabel";
             this.linkLabel.Size = new System.Drawing.Size(180, 13);
             this.linkLabel.TabIndex = 6;
@@ -176,12 +181,56 @@ namespace presage_wcf_service_system_tray
             this.linkLabel.Text = "http://presage.sourceforge.net";
             this.linkLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.licenseButton, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.closeButton, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(10, 336);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(180, 29);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // licenseButton
+            // 
+            this.licenseButton.AutoSize = true;
+            this.licenseButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.licenseButton.Location = new System.Drawing.Point(3, 3);
+            this.licenseButton.Name = "licenseButton";
+            this.licenseButton.Size = new System.Drawing.Size(54, 23);
+            this.licenseButton.TabIndex = 0;
+            this.licenseButton.Text = "License";
+            this.licenseButton.UseVisualStyleBackColor = true;
+            this.licenseButton.Click += new System.EventHandler(this.licenseButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeButton.AutoSize = true;
+            this.closeButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.closeButton.Location = new System.Drawing.Point(134, 3);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(43, 23);
+            this.closeButton.TabIndex = 1;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
             // AboutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(200, 350);
+            this.ClientSize = new System.Drawing.Size(200, 375);
             this.Controls.Add(this.flowLayoutPanel1);
             this.MaximizeBox = false;
             this.Name = "AboutForm";
@@ -189,6 +238,8 @@ namespace presage_wcf_service_system_tray
             this.flowLayoutPanel1.ResumeLayout(false);
             this.flowLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.presageLogoBox)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -203,5 +254,8 @@ namespace presage_wcf_service_system_tray
         private System.Windows.Forms.Label disclaimerLabel;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.LinkLabel linkLabel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button licenseButton;
+        private System.Windows.Forms.Button closeButton;
     }
 }
